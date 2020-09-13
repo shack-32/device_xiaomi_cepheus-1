5f00b226bb371c7c8dbf34655fde48234dae0713
EcrosoftXiao <xy19990903@gmail.com>
2020-09-13 10:14:35 +0000

    cepheus: fix extract scripts
    Change-Id: I401df2c842897c261fe7612801bb6c46a500d8ed

--- extract-files.sh
+++ extract-files.sh
@@ -26,7 +26,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 RR_ROOT="${MY_DIR}"/../../..
 
-HELPER="${RR_ROOT}/vendor/rr/build/tools/extract_utils.sh"
+HELPER="${RR_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -86,11 +86,6 @@ extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
-
-for i in $(grep -rn 'libhidltransport.so\|libhwbinder.so' ../../../vendor/xiaomi/"${DEVICE_COMMON}"/proprietary | awk '{print $4}'); do
-	patchelf --remove-needed "libhwbinder.so" "$i"
-	patchelf --remove-needed "libhidltransport.so" "$i"
-done
 
 for i in $(grep -rn 'libhidltransport.so\|libhwbinder.so' ../../../vendor/xiaomi/"${DEVICE}"/proprietary | awk '{print $4}'); do
 	patchelf --remove-needed "libhwbinder.so" "$i"
